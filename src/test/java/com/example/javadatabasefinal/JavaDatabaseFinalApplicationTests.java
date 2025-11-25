be75b527ec86be53c4263be4268127897baabf5e
mahammadisaev1 <mahammadisaev1@gmail.com>
2025-11-25 14:26:53 +0400

    Initial Spring Boot project

---
+++ src/test/java/com/example/javadatabasefinal/JavaDatabaseFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javadatabasefinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaDatabaseFinalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
